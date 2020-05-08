dd6abcd1fbc84e3340bd783b71938b4b9a3bd821
Joseph M <jmedina1985@hotmail.com>
2020-05-08 00:05:21 -0700

    Customer Error Handler and ErrorResponse class added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
 // Load env vars
@@ -25,6 +26,9 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+
+// Middleware has to come after mount router
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
