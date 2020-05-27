ad645136f67192c4ae1af9b0cea76f3e8d8b80ae
Joseph M <jmedina1985@hotmail.com>
2020-05-27 00:22:18 -0700

    Added API security

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,10 +11,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(" ")[1];
+    // Set token from cookie
   }
-
-  // else if(req.cookies.token) {
+  // else if (req.cookies.token) {
   //   token = req.cookies.token;
   // }
 
